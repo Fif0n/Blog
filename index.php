c9e3bee6ad5dda8ad0e7a3dd754fcc15a029a98a
fifon <filip_michalczyk@wp.pl>
2020-01-28 12:45:59 +0100

    changed main layout

--- index.php
+++ index.php
@@ -5,39 +5,41 @@
     <main>
         <h2>Najnowsze posty</h2>
         <div class="latest-posts">
-            <div class="last-post">
-                <div class="post-card-big">
-                    <!-- <img src="img/golem.png" alt=""> -->
-                    <h4>Ulok</h4>
+            <div class="post-card">
+                <img src="img/malpa.jpg" alt="">
+                <div class="post-title">
+                    <h3>lorem ipsum</h3>
                 </div>
             </div>
-            <div class="last-posts">
-                <div class="post-card-right">
-                    <!-- <img src="img/golem.png" alt=""> -->
-                    <h4>Ulok</h4>
-                </div>
-                <div class="post-card-right">
-                    <!-- <img src="img/golem.png" alt=""> -->
-                    <h4>Ulok</h4>
+            <div class="post-card">
+                <img src="img/malpa.jpg" alt="">
+                <div class="post-title">
+                    <h3>Lorem ipsum</h3>
                 </div>
             </div>
-        </div>
-        <div class="other-posts">
             <div class="post-card">
-                <!-- <img src="img/golem.png" alt=""> -->
-                <h4>Ulok</h4>
+                <img src="img/malpa.jpg" alt="">
+                <div class="post-title">
+                    <h3>Lorem ipsum</h3>
+                </div>
             </div>
             <div class="post-card">
-                <!-- <img src="img/golem.png" alt=""> -->
-                <h4>Ulok</h4>
+                <img src="img/malpa.jpg" alt="">
+                <div class="post-title">
+                    <h3>Lorem ipsum</h3>
+                </div>
             </div>
             <div class="post-card">
-                <!-- <img src="img/golem.png" alt=""> -->
-                <h4>Ulok</h4>
+                <img src="img/malpa.jpg" alt="">
+                <div class="post-title">
+                    <h3>Lorem ipsum</h3>
+                </div>
             </div>
             <div class="post-card">
-                <!-- <img src="img/golem.png" alt=""> -->
-                <h4>Ulok</h4>
+                    <img src="img/malpa.jpg" alt="">
+                <div class="post-title">
+                    <h3>Lorem ipsum</h3>
+                </div>
             </div>
         </div>
     </main>
