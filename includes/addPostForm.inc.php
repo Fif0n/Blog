d72ba429ad9a469a6722983aae500c42392796e2
Fifon <filip_michalczyk@wp.pl>
2020-03-01 11:19:23 +0100

    added add post form

--- includes/addPostForm.inc.php
+++ includes/addPostForm.inc.php
@@ -1,6 +1,16 @@
 <?php
 if($_SESSION['userRank'] == 'admin'){
-    
+?>
+    <main>
+        <h2>Dodaj post</h2>
+        <form action="/blog/includes/addPost.inc.php" method="POST" class="add" enctype="multipart/form-data">
+            <input type="text" name="title" placeholder="Tytuł">
+            <textarea name="" id="" cols="40" rows="15">Tutaj wpisz treść...</textarea>
+            <input type="file" name="file">
+            <button type="submit" name="add-submit">Dodaj post</button>
+        </form>
+    </main>
+<?php
 } else {
     header("Location: /blog/index/home");
 }
