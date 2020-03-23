11842e271c86969e5f8996fc033fba44e5e1a3fd
Fifon <filip_michalczyk@wp.pl>
2020-03-23 12:35:31 +0100

    created layout for comment section

--- includes/post.inc.php
+++ includes/post.inc.php
@@ -19,13 +19,20 @@
                             <p>'.$row['content'].'</p>
                         </div>
                         <h2>Komentarze</h2>
-                        <div class="comments">
+                        <div class="comments-section">
                             <form>
-                                <input type="text" name="comment" placeholder="Dodaj komentarz">
+                                <input type="text" name="comment" placeholder="Dodaj komentarz jako '.$_SESSION['username'].'">
+                                <button type="submit" name="comment-submit">Dodaj komentarz</button>
                             </form>
-                        </div>'
-                        
-                    ;
+                            <div class="comment">
+                                <p>Ktoś napisał:</p>
+                                <h4>lorem ipsum elo benc pozdro 6000000000000000000000000000000000 xd. Wisz o co chodzi mordooooo.</h3>
+                            </div>
+                            <div class="comment">
+                                <p>Ktoś napisał:</p>
+                                <h4>No proste ziomuś. równo na rejonie </h3>
+                            </div>
+                        </div>';
                 }
             }
         ?>
