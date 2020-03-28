bffb8dc05ea0b34aa547f5e639de2fbd8e096c2f
Fifon <filip_michalczyk@wp.pl>
2020-03-28 12:21:30 +0100

    fixed some bugs

--- includes/post.inc.php
+++ includes/post.inc.php
@@ -17,14 +17,22 @@
                             <hr/>
                             <img src="/blog/img/'.$row['imgName'].'"/>
                             <p>'.$row['content'].'</p>
-                        </div>
-                        <h2>Komentarze</h2>
-                        <div class="comments-section">
-                            <form>
+                        </div>';
+                        
+                }
+                echo '<h2>Komentarze</h2>
+                        <div class="comments-section">';
+                        if(isset($_SESSION['id'])){
+                            echo '<form>
                                 <input type="text" name="comment" placeholder="Dodaj komentarz jako '.$_SESSION['username'].'">
                                 <button type="submit" name="comment-submit">Dodaj komentarz</button>
-                            </form>
-                            <div class="comment">
+                            </form>';
+                        } else {
+                            echo '<div class="needLogin">
+                                    <h4>Musisz się zalogować aby dodać kometarz</h4>
+                                </div>';
+                        } 
+                            echo '<div class="comment">
                                 <p>Ktoś napisał:</p>
                                 <h4>lorem ipsum elo benc pozdro 6000000000000000000000000000000000 xd. Wisz o co chodzi mordooooo.</h3>
                             </div>
@@ -33,7 +41,6 @@
                                 <h4>No proste ziomuś. równo na rejonie </h3>
                             </div>
                         </div>';
-                }
             }
         ?>
     </div>
